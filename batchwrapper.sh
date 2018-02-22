43f576404952cf23752fe9137e6ca520c07162cc
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-02-22 10:59:41 -0800

    add shell wrapper for rhinos

---
+++ batchwrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Make a symlink to this script
+# (or copy it) to /app/bin/batchwrapper.
+
+source /etc/profile.d/fh_path.sh
+
+#
+# remove local user-customisations to python behavior
+unset PYTHONPATH
+unset PYTHONUSERBASE
+unset PYTHONHOME
+
+PYTHON=/app/local/fredhutch_batch_wrapper/env/bin/python
+SCRIPT=/app/local/fredhutch_batch_wrapper/env/bin/batchwrapper
+
+ml Python/3.6.4-foss-2016b-fh1
+$PYTHON $SCRIPT $@
